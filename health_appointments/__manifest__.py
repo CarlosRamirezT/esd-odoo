f77e75846d352b8ba7f5098251d37d2b9babdc69
Carlos <carlosdanielrt@gmail.com>
2024-02-05 06:15:15 -0400

    [ADD] health_appointments: patient views

--- health_appointments/__manifest__.py
+++ health_appointments/__manifest__.py
@@ -15,7 +15,8 @@
         "security/health_security.xml",
         "security/ir.model.access.csv",
         "views/health_specialty.xml",
-        "views/health_doctors.xml"
+        "views/health_doctors.xml",
+        "views/health_patients.xml"
     ],
     "demo": [
         "demo/demo_health_specialty.xml",
